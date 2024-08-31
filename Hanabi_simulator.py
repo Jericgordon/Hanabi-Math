b88c2c217019628a28f36c93acd59ab20308e515
Jen Gordon <86987150+Jericgordon@users.noreply.github.com>
2024-08-31 12:56:49 -0400

    Revised and refined into Class. Made speed changes

--- Hanabi_simulator.py
+++ Hanabi_simulator.py
@@ -1,130 +1,204 @@
-import random
-from termcolor import colored
+import random #used for shuffling the deck
+import copy #used for makeing a copy of a the deck to shuffle
+from termcolor import colored #used for printing Hanabi boards in readable output
+import bisect #used for inserting into a sorted list
+import unittest #testing
 
 
 def main():
-    games = 1000000
+    games = 100000
     wins = 0
+    h = Hanabi_game(5,False)
+
     for _ in range(games):
-        if play_game(5):
+        if h.play_game():
             wins += 1
 
     print(f"{wins} out of {games} games were won. A win rate of {wins/games}")
-    # print_visual_cards([3, 7, 12, 5, 17, 8, 20, 11, 1, 2, 18, 5])
-    # print_play_base([-1, 4, 9, 15, 22])
 
-def is_essential(discards:list, play_base:list, card:int) -> bool:
-    if ((card % 5) == 4):
+
+
+class Hanabi_game():
+    def __init__(self,colors:int,print_lost_games) -> None:
+        #user preferences
+        self.print_lost_games = print_lost_games
+        self.debug = False
+        self.colors = colors
+
+        #built in options
+        self.color_order ={0:"red",1:'yellow',2:'green',3:'blue',4:'white',5:'magenta'}
+        self.discards_reference = {x:0 for x in range(1,(self.colors * 5) + 1)}
+        self.play_base_reference = [x for x in range(0,(self.colors) * 5,5)]
+        self.deck_reference = self._make_deck(colors)
+
+        
+
+    def _setup_new_game(self):
+        self.discards = copy.deepcopy(self.discards_reference)
+        self.play_base = copy.deepcopy(self.play_base_reference)
+        self.hand = []
+        #shuffle a new deck
+        self.deck = copy.deepcopy(self.deck_reference)
+        random.shuffle(self.deck)
+        for _ in range(10):
+            self.hand.append(self.deck.pop())
+
+    #A unique card is card (color_number * 5) + card_number
+    #e.g. if blue is 2, the blue one is (2 * 5) + 2 or 12
+    def _make_deck(self,colors) -> list:
+        deck = []
+        for color in range(colors):
+            #Add ones
+            for _ in range(3):
+                deck.append((color * 5) + 1)
+            #Add 2-4
+            for card_number in range(2,5):
+                for _ in range(2):
+                    deck.append((color * 5) + card_number)
+            #Add 5s
+            deck.append((color * 5) + 5)
+        return deck
+        
+    def play_game(self) -> int:
+        self._setup_new_game()
+        while len(self.deck) > 0:
+            played = False
+            could_discard = []
+            index_reference = {}
+
+            #go through the hand; play any playable card; keep track of other cards
+            for index in range(10):
+                if self.is_playable(self.hand[index]):
+                    self.play_card(self.hand[index])
+                    #print(f"Played {hand[index]} on {play_base}")
+                    self.hand[index] = self.deck.pop() #Draw a new card. This version does not have hand order
+                    played = True
+                    break #stop going through the hand
+
+                if not self.is_essential(self.hand[index]):
+                    bisect.insort(could_discard,self.hand[index])
+                    index_reference[self.hand[index]] = index
+
+            #check to see if we've already played a card
+            if played == True:
+                continue 
+
+            elif (played == False) and (len(could_discard) > 0):
+                card = could_discard[-1]
+                hand_index = index_reference[card]
+                self.hand[hand_index] = self.deck.pop()
+                self.discards[card] += 1
+                continue
+            
+            else:
+                #debugging printing 
+                if self.print_lost_games:
+                    print("Game lost")
+                    self.print_visual_hand()
+                    self.print_visual_play_base()
+                    self.print_visual_discard()
+                
+                if self.debug:
+                    print(f"hand: {self.hand}")
+                    print(f"Play base {self.play_base}")
+                    print(f"Discards: {self.discards}")
+
+                return False
         return True
     
-    if (play_base[card // 5] >= card):
+    #helper functions for the game
+    def is_essential(self,card:int) -> bool:
+        if ((card % 5) == 0):
+            return True
+        
+        if (self.play_base[(card - 1) // 5] >= card):
+            return False
+        
+        #we don't need to worry about the fact that there are 3 ones in the deck for the following,
+        #because they're alwayas playable or discardable. They'll either be caught by the above expression
+        #or be directly played. This would not be the case if there were 3 ones
+        if (self.discards[card] == 1):
+            return True
+        return False
+
+    def is_playable(self,card:int):
+        color = (card-1) // 5
+        if self.play_base[color] == (card - 1):
+            return True
         return False
     
-    if card in discards:
-        return True
+    def play_card(self,card:int):
+        self.play_base[(card - 1)//5] += 1
+
+    def print_visual_play_base(self):
+        print("Play Base: ",end="")
+        for index in range(self.colors):
+            card = self.play_base[index]
+            color = self.color_order[index]
+            if card == self.play_base_reference[index]:
+                number = 0
+            else:
+                number = ((card -1 ) % 5) + 1
+            print(colored(number,color) + " ",end="")
+        print("")
+
+    def print_visual_hand(self):
+        self.hand.sort()
+        print("hand: ",end="")
+        for card in self.hand:
+            color = self.color_order[(card -1)//5]
+            number = card % 5
+            if number == 0:
+                number = 5
+            print(colored(number,color) + " ",end="")
+        print("")
     
-    return False
+    def print_visual_discard(self):
+        print("Discards: ",end="")
+        for card in self.discards_reference:
+            if self.discards[card] > 0:
+                color = self.color_order[(card -1) // 5]
+                number = card % 5
+                if number == 0:
+                    number == 5
+                print(colored(number,color) + " ",end="")
+        print("")
 
-def is_playable(card:int, play_base):
-    
-    color = card // 5
-    if play_base[color] == (card - 1):
-        return True
-    return False
 
-def play_game(colors) -> int:
-    #print("---New Game----")
-    deck = make_deck(colors)
-    hand = [] #create a list of cards to add into the hand representing both hands
-    discard = [] #list of cards discarded
-    play_base = [x for x in range(-1,(colors-1) * 5,5)] # -> [0,5,10] 
-    #print(f"Play Base: {play_base}")
 
-    #deal hand
-    for _ in range(10):
-        hand.append(deck.pop())
 
-    while len(deck) > 0:
-        played = False
-        for index in range(10):
-            if is_playable(hand[index],play_base):
-                play_base[(hand[index] // 5)] += 1
-                #print(f"Played {hand[index]} on {play_base}")
-                hand[index] = deck.pop()
-                played = True
-                
-                break
+class Hanabi_tests(unittest.TestCase):
+
+    def test_is_playbable_empty_board(self):
+        #test the normal case of playable cards on an empty board
+        h5 = Hanabi_game(5)
+        h5.play_base = [0,5,10,15,20]
+        valid_plays = [1,6,11,16,21]
+        invalid_plays = []
+        for number in range(1,26):
+            if number not in valid_plays:
+                invalid_plays.append(number)
+        for play in valid_plays:
+            self.assertTrue(h5.is_playable(play))
+
+        for play in invalid_plays:
+            self.assertFalse(h5.is_playable(play))
+
+    def test_is_playble_wrong_color(self):
+        h5 = Hanabi_game(5)
+        h5.play_base = [5,6,10,15,20]
+        self.assertFalse(h5.is_playable(6))
         
-        #check to see if we've already played a card
-        if played == True:
-            continue
-
-        #see if there's a card that we could get
-        for index in range(10):
-            if not is_essential(discard,play_base,hand[index]):
-                discard.append(hand[index])
-                hand[index] = deck.pop()
-                played = True
-                break
-        
-        if played == False:
-            # print("Game lost")
-            # print(f"could not play any of {hand} on {play_base}")
-            # print("hand: ",end="")
-            # print_visual_cards(hand)
-            # print("Play Base: ",end="")
-            # print_play_base(play_base)
-            # print(f"The discard was: ",end="")
-            # print_visual_cards(discard)
-            return False
-    
-    #print("Game won")
-    return True
-
-
-
-def print_play_base(to_print):
-    color_order = {0:"red",1:'yellow',2:'green',3:'blue',4:'white'}
-    for index in range(len(to_print)):
-        card = to_print[index] + 1
-        color = color_order[(card) // 5]
-        number = (card) % 5
-        print(colored(number,color) + " ",end="")
-    print("")
-
-def print_visual_cards(to_print:list):
-    color_order = {0:"red",1:'yellow',2:'green',3:'blue',4:'white'}
-    to_print.sort()
-    for card in to_print:
-        color = color_order[card//5]
-        number = card % 5
-        print(colored(number + 1,color) + " ",end="")
-    print("")
-
-
-
-
-
-
-#A unique card is card (color_number * 5) + card_number
-#e.g. if blue is 2, the blue one is (2 * 5) + 2 or 12
-def make_deck(colors) -> list:
-    deck = []
-    for color in range(colors):
-        #Add ones
-        for _ in range(3):
-            deck.append((color * 5) + 0)
-        #Add 2-4
-        for card_number in range(2,5):
-            for _ in range(2):
-                deck.append((color * 5) + card_number - 1)
-        #Add 5s
-        deck.append((color * 5) + 4)
-    random.shuffle(deck)
-    return deck
-
-
-
+    def test_play_card(self):
+        h5 = Hanabi_game(5)
+        h6 = Hanabi_game(6)
+        h5.play_base = [0,5,10,15,20]
+        h5.play_card(16)
+        expected = [0,5,10,16,20]
+        self.assertEqual(expected,h5.play_base)
+        h5.play_card(21)
+        expected = [0,5,10,16,21]
+        self.assertEqual(expected,h5.play_base)
 
 
 
@@ -132,4 +206,6 @@ def make_deck(colors) -> list:
 
 
 if __name__ == "__main__":
+    #unittest.main()
     main()  
+
