80db67c4d57a80cb5dac7b968a02e596b7feaadf
Jen Gordon <86987150+Jericgordon@users.noreply.github.com>
2024-12-06 09:58:12 -0700

    minor changes

---
+++ Strategy_graphs.py
@@ -0,0 +1,10 @@
+class Strategy_graphs():
+    def __init__(self):
+        ...
+        
+    def add_graph():
+        ...
+
+    def graph_all():
+        ...
+
--- main.py
+++ main.py
@@ -2,22 +2,27 @@ from Hanabi_simulator import Hanabi_game
 from Strategy import Strategy
 from Cheating_strategy import Cheating_strategy
 import matplotlib.pyplot as plt
+import math
 
 def main():
     iterations = 1
-    score = [0 for _ in range(26)] 
+    score_cheating_strategy = [0 for _ in range(26)] 
     s = Hanabi_game(5)
     s.debug = True
     for _ in range(iterations):
-        score[s.play_game(Strategy)] += 1   
+        score_cheating_strategy[s.play_game(Strategy)] += 1   
     x_axis = [x for x in range(0,26)]
-    average_score = 0
-    for val_index in range(len(score)):
-        average_score += (val_index * score[val_index])
+    average_score = sum (x_axis)
+    for val_index in range(len(score_cheating_strategy)):
+        average_score_cheating_strategy += (val_index * score_cheating_strategy[val_index])
     average_score = average_score / iterations
     print(average_score)
-    plt.bar(x_axis,score)
+
+
+    plt.bar(x_axis,score_cheating_strategy)
     plt.show()
+
+
     
 if __name__ == '__main__':
     main()
