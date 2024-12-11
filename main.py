617b76b86904008c50b223dfde458edf89c81796
Jen Gordon <86987150+Jericgordon@users.noreply.github.com>
2024-12-11 10:36:11 -0700

    Changed main to reflect running variable costs for playing cards; all function edited to allow that.

--- main.py
+++ main.py
@@ -1,37 +1,47 @@
 from Hanabi_simulator import Hanabi_game
-from Cheating_strategies import Cheating_play,Cheating_play_discard
+from Cheating_strategies import Cheating_play,Cheating_play_discard,Cheating_clue_cost
 import matplotlib.pyplot as plt
 import numpy as np
 
 
 
 def main():
-    iterations = 10000
-    score_play_and_discard = [0 for _ in range(26)] 
-    score_play = [0 for _ in range(26)] 
+    iterations = 100000
+    #build the list of things considered
+    play_cost_considered = [] 
+    min = 0
+    max = 2.1
+    step = .25
+    while min < max:
+        play_cost_considered.append(min)
+        min += step
+
+    results = [[0 for _ in range(26)] for _ in range(len(play_cost_considered))] 
     s = Hanabi_game(5)
     #s.debug = True 
     for _ in range(iterations):
-        score_play_and_discard[s.play_game(Cheating_play_discard)] += 1 
-        score_play[s.play_game(Cheating_play)] += 1 
-        
-    x_axis = [x for x in range(0,26)]
-    total_points_play_discard = 0
-    total_points_play = 0
-    for val_index in range(len(score_play_and_discard)):
-        total_points_play_discard += (val_index * score_play_and_discard[val_index])
-        total_points_play += (val_index * score_play[val_index])
-
-    average_score_play_discard = total_points_play_discard / iterations
-    average_score_play = total_points_play /iterations
-    print("average score playing and discarding:",average_score_play_discard)
-    print("average score playing",average_score_play)
-
-    width = .25
-    r = np.arange(26)
+        for cost_index in range(len(play_cost_considered)):
+            s.clues_per_play = play_cost_considered[cost_index]
+            results[cost_index][s.play_game(Cheating_clue_cost)] += 1
+        #score_play[s.play_game(Cheating_play)] += 1 
     
-    plt.bar(r,score_play_and_discard,color = 'b',width=.5)
-    plt.bar(r + width,score_play,color = 'g',width=.25)
+    cost_averages = {}
+    for score_index in range(len(play_cost_considered)):
+        sum_score = 0
+        for val_index in range(len(results[0])):
+            sum_score += (val_index * results[score_index][val_index])
+        cost_averages[play_cost_considered[score_index]] = (sum_score / iterations)
+       
+    for key,value in cost_averages.items():
+        print(f"Average Score of play cost {key}:{value}")
+
+    
+    r = np.arange(26)
+    for scores_index in range(len(play_cost_considered)):
+        width = .1 * scores_index
+        plt.bar(r + width,results[scores_index],label=play_cost_considered[scores_index],width=.1)
+        
+    plt.legend()
     plt.show()
 
 
